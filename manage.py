fe8e0a2738d7e2e1ba731d2389247cb2dea20279
Nataliia Uvarova <grafinya.uvarova@gmail.com>
2014-03-07 12:42:59 +0100

    Resolve #4. Re-generate manifest.appcache during deploy from template

--- manage.py
+++ manage.py
@@ -1,9 +1,26 @@
+import datetime
+
+from jinja2 import Environment, FileSystemLoader
 from flask.ext.script import Manager
 
 from server import create_app
 
 
 manager = Manager(create_app)
+
+
+@manager.command
+def generate_manifest(out_path=None):
+    """ Generate manifest.appcache from the templates/manifest.appcache.jinja"""
+    now = datetime.datetime.now()
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template('manifest.appcache.jinja')
+    manifest = template.render(date=now)
+    if out_path:
+        with open(out_path, 'w') as out:
+            out.write(manifest)
+    else:
+        print manifest
 
 
 if __name__ == "__main__":
