ec7c6fc4000c0167a2ec3984bdf2e1c066ae4de5
Nataliia Uvarova <grafinya.uvarova@gmail.com>
2014-03-07 10:33:42 +0100

    When click on map, popup with note creation form appears

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -2,10 +2,32 @@
 
 var controllers = angular.module("geonoteControllers", ["StorageServices"])
 
+controllers.controller("CreateNoteModalController", ["$scope", "NotesStorage", "$modalInstance", "latlng",
+    function ($scope, NotesStorage, $modalInstance, latlng) {
+  $scope.isModal = true;
+  $scope.noteContent = {};
+  $scope.hasCoords = true;
+  $scope.noteContent.lat = latlng.lat;
+  $scope.noteContent.lng = latlng.lng;
 
-controllers.controller("CreateNoteController", ["$scope", "$injector", "NotesStorage",
-    function ($scope, $injector, NotesStorage) {
-  $scope.isModal = $injector.has("$modalInstance");
+  $scope.ok = function (form) {
+    if(form.$invalid) {
+      return;
+    }
+    $scope.noteContent.date = new Date().toISOString();
+
+    NotesStorage.addNote($scope.noteContent);
+    $modalInstance.close($scope.noteContent);
+  };
+
+  $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+  };
+}]);
+
+controllers.controller("CreateNoteController", ["$scope", "NotesStorage",
+    function ($scope, NotesStorage) {
+  $scope.isModal = false;
   $scope.noteContent = {};
   $scope.hasCoords = false;
 
@@ -16,18 +38,8 @@ controllers.controller("CreateNoteController", ["$scope", "$injector", "NotesSto
     $scope.noteContent.date = new Date().toISOString();
 
     NotesStorage.addNote($scope.noteContent);
-    if($scope.isModal) {
-      $injector.get("$modalInstance").close($scope.noteContent);
-    } else {
-      $scope.noteContent = {};
-      form.$setPristine();
-    }
-  };
-
-  $scope.cancel = function () {
-    if($scope.isModal) {
-      $injector.get("$modalInstance").dismiss('cancel');
-    }
+    $scope.noteContent = {};
+    form.$setPristine();
   };
 
   window.navigator.geolocation.watchPosition(function(pos) {
@@ -40,8 +52,8 @@ controllers.controller("CreateNoteController", ["$scope", "$injector", "NotesSto
 }]);
 
 
-controllers.controller("MapViewController", ["$scope", "NotesStorage",
-    function ($scope, NotesStorage) {
+controllers.controller("MapViewController", ["$scope", "NotesStorage", "$modal",
+    function ($scope, NotesStorage, $modal) {
   angular.extend($scope, {
     events: {
       map: {
@@ -55,4 +67,17 @@ controllers.controller("MapViewController", ["$scope", "NotesStorage",
   });
 
   $scope.markers = NotesStorage.markers;
+
+  $scope.$on('leafletDirectiveMap.click', function(event, args) {
+    var latlng = args.leafletEvent.latlng;
+    var modalInstance = $modal.open({
+      templateUrl: 'partials/note_form.html',
+      controller: "CreateNoteModalController",
+      resolve: {
+        latlng: function () {
+          return latlng;
+        },
+      }
+    });
+  });
 }]);
